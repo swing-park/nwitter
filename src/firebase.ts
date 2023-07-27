2541e841aa3951981746bcaba37631b56d4821cc
swing-park <ongsim123@gmail.com>
2023-07-27 10:30:53 +0900

    :hammer: Fix : firebase config file
    - 불필요한 import 삭제

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import "firebase/compat/firestore";
-import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
